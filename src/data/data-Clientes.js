945ad7e5a9b49066492d9c3d040921f48e1805dd
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2020-11-10 12:07:31 +0100

    Update data

--- src/data/data-Clientes.js
+++ src/data/data-Clientes.js
@@ -3,7 +3,7 @@
 db.Clientes.InsertMany([
     {"_id": 201, "Nombre":"ALCARAZ VERDUZCO MANUEL", "DNI": 475139854,  "FechaPedido": 2018-09-23, "FechaEntrega": 2018-09-30, "DineroGastado": 173,},
     {"_id": 202, "Nombre":"ALEXANDERSON ROSAS ERICK", "DNI": 781496578,  "FechaPedido": 2018-07-03, "FechaEntrega": 2018-07-13, "DineroGastado": 83,},
-    {"_id": 203, "Nombre":"ALONSO	VANEGAS	MARIO ARTURO", "DNI": 167945239,  "FechaPedido": 2018-11-13, "FechaEntrega": 2018-11-21, "DineroGastado": 164,},
+    {"_id": 203, "Nombre":"ALONSO VANEGAS MARIO ARTURO", "DNI": 167945239,  "FechaPedido": 2018-11-13, "FechaEntrega": 2018-11-21, "DineroGastado": 164,},
     {"_id": 204, "Nombre":"ÁLVAREZ LEFFMANS FRANCISCO JAVIER", "DNI": 586974136,  "FechaPedido": 2018-02-07, "FechaEntrega": 2018-02-15, "DineroGastado": 73,},
     {"_id": 205, "Nombre":"APIQUIAN GUITART ROGELIO", "DNI": 589648139,  "FechaPedido": 2018-12-01, "FechaEntrega": 2018-12-05, "DineroGastado": 86,},
     {"_id": 206, "Nombre":"ARREDONDO GALÁN JOSÉ HUGO", "DNI": 763457981,  "FechaPedido": 2018-01-30, "FechaEntrega": 2018-02-04, "DineroGastado": 28,},
@@ -12,5 +12,5 @@ db.Clientes.InsertMany([
     {"_id": 209, "Nombre":"BECKER FAUSER INGEBORG", "DNI": 450698723, "FechaPedido": 2018-03-17,  "FechaEntrega": 2018-03-21, "DineroGastado": 90,},
     {"_id": 210, "Nombre":"BERUMEN CAMPOS JAIME", "DNI": 485105497, "FechaPedido": 2018-08-09, "FechaEntrega": 2018-08-16, "DineroGastado": 110,},
     {"_id": 211, "Nombre":"BOJALIL PARRA RAFAEL", "DNI": 468954961, "FechaPedido": 2018-05-28, "FechaEntrega": 2018-06-02, "DineroGastado": 37,},
-    {"_id": 212, "Nombre":"CAMPILLO	SERRANO	CARLOS DANIEL", "DNI": 489171287, "FechaPedido": 2018-02-01, "FechaEntrega": 2018-02-09, "DineroGastado": 34,},
+    {"_id": 212, "Nombre":"CAMPILLO SERRANO CARLOS DANIEL", "DNI": 489171287, "FechaPedido": 2018-02-01, "FechaEntrega": 2018-02-09, "DineroGastado": 34,},
 ])
--- src/data/data-Productos.js
+++ src/data/data-Productos.js
@@ -5,29 +5,29 @@ db.Productos.InsertMany([
         "Ancho": 0.8,
         "Largo": 1,
         "Calidad": 8,},
-    "Vendido": true, "Descripcion": "",},
+    "Vendido": true, "Descripcion": "Paneles de acero en buen estado. Se compran por paquetes de 10. Envío gratis a partir de los 50€.",},
 
     {"_id": "PDH", "Nombre": "Panel de hierro", "Precio": 15, "Cantidad": 32, "Caracteristicas": {
         "Ancho": 1,
         "Largo": 1,
         "Calidad": 7,},
-    "Vendido": true, "Descripcion": "",},
+    "Vendido": true, "Descripcion": "Paneles de hierro para la construcción. Fuertes y resistentes. De la empresa FOUNTAIN SA.",},
 
     {"_id": "PDAL", "Nombre": "Panel de aluminio", "Precio": 25, "Cantidad": 15, "Caracteristicas": {
         "Ancho": 0.6,
         "Largo": 1,
         "Calidad": 9,},
-    "Vendido": false, "Descripcion": "",},
+    "Vendido": false, "Descripcion": "Paneles de aluminio para construcción e investigación. Se recomienda guardar con cuidado. Envío gratis a partir de los 100€.",},
 
     {"_id": "BDAL", "Nombre": "Barras de aluminio", "Precio": 30, "Cantidad": 20, "Caracteristicas": {
         "Ancho": 0.2,
         "Largo": 1.5,
         "Calidad": 8,},
-    "Vendido": true, "Descripcion": "",},
+    "Vendido": true, "Descripcion": "Barras de aluminio. Envío gratis si se piden junto a otro producto.",},
 
     {"_id": "", "Nombre": "Tornillos de acero", "Precio": 0.1, "Cantidad": 1000, "Caracteristicas": {
         "Ancho": 0.02,
         "Largo": 0.08,
         "Calidad": 5,},
-    "Vendido": false, "Descripcion": "",},    
+    "Vendido": false, "Descripcion": "Tornillos de acero. El envío tarda más de lo normal debido a que se importan desde Francia.",},    
 ])
