9d4736cf5ab06b0dfa69d8dfcb34761023e66ee6
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2020-11-22 16:21:41 +0100

    Update query-clientes

--- src/query/query-Clientes.js
+++ src/query/query-Clientes.js
@@ -5,7 +5,7 @@ db.Clientes.find({$and:[
     {"FechaPedido": {$gte: new Date(2018-03-06,00,00,000+00,00)}},
     {"DineroGastado": {$lte: 50}},
     {"_id": {$ne: 210}}
-]})
+]}).pretty()
 
 
 /*Busca los clientes que o no empiecen por B y contengan una G y una A, o hayan gastado exactamente 164.*/
@@ -15,12 +15,12 @@ db.Clientes.find({$or:[
         {"Nombre": {$regex: /G\BA\B/}}
     ]},
     {"DineroGastado": {$eq: 164}}
-]})
+]}).pretty()
 
 
-/*Busca todos los clientes que no se llamen Erick ni su dinero gastado está entre Ñ*/
+/*Cuenta todos los clientes que no se llamen Erick ni su dinero gastado está entre 50 y 100.*/
 db.Clientes.find({$nor:[
     {"Nombre": {$regex: /ERICK/}},
     {"DineroGastado": { $gte: 50, $lt: 100 }}
-]})
+]}).count()
 
