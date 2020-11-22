0f50715d241f26b0b446325478f57555bb9869ce
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2020-11-22 16:09:56 +0100

    Update data-Productos

--- src/data/data-Productos.js
+++ src/data/data-Productos.js
@@ -6,33 +6,33 @@ db.Productos.insertMany([
         "Ancho": 0.8,
         "Largo": 1,
         "Calidad": 8,},
-    "Vendido": true, "Descripcion": "Paneles de acero en buen estado. Se compran por paquetes de 10. Envío gratis a partir de los 50€."}
+    "Vendido": true, "Etiquetas": [18, 19, 20], "Descripcion": "Paneles de acero en buen estado. Se compran por paquetes de 10. Envío gratis a partir de los 50€."}
             ,
     {"_id": "PDH", "Nombre": "Panel de hierro", "Precio": 15, "Cantidad": 32, "Caracteristicas": 
         {
         "Ancho": 1,
         "Largo": 1,
         "Calidad": 7,},
-    "Vendido": true, "Descripcion": "Paneles de hierro para la construcción. Fuertes y resistentes. De la empresa FOUNTAIN SA."}
+    "Vendido": true, "Etiquetas": [11, 12, 13], "Descripcion": "Paneles de hierro para la construcción. Fuertes y resistentes. De la empresa FOUNTAIN SA."}
             ,
     {"_id": "PDAL", "Nombre": "Panel de aluminio", "Precio": 25, "Cantidad": 15, "Caracteristicas": 
         {
         "Ancho": 0.6,
         "Largo": 1,
         "Calidad": 9,},
-    "Vendido": false, "Descripcion": "Paneles de aluminio para construcción e investigación. Se recomienda guardar con cuidado. Envío gratis a partir de los 100€."}
+    "Vendido": false, "Etiquetas": [8, 9, 10], "Descripcion": "Paneles de aluminio para construcción e investigación. Se recomienda guardar con cuidado. Envío gratis a partir de los 100€."}
             ,
     {"_id": "BDAL", "Nombre": "Barras de aluminio", "Precio": 30, "Cantidad": 20, "Caracteristicas": 
         {
         "Ancho": 0.2,
         "Largo": 1.5,
         "Calidad": 8,},
-    "Vendido": true, "Descripcion": "Barras de aluminio. Envío gratis si se piden junto a otro producto."}
+    "Vendido": true, "Etiquetas": [3, 4, 5], "Descripcion": "Barras de aluminio. Envío gratis si se piden junto a otro producto."}
             ,
     {"_id": "TDA", "Nombre": "Tornillos de acero", "Precio": 0.1, "Cantidad": 1000, "Caracteristicas": 
         {
         "Ancho": 0.02,
         "Largo": 0.08,
         "Calidad": 5,},
-    "Vendido": false, "Descripcion": "Tornillos de acero. El envío tarda más de lo normal debido a que se importan desde Francia."}
+    "Vendido": false, "Etiquetas": [24, 25, 26], "Descripcion": "Tornillos de acero. El envío tarda más de lo normal debido a que se importan desde Francia."}
 ]);
