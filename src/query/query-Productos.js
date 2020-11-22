884e627a7005203b78807902e686cb0e16019205
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2020-11-22 16:06:05 +0100

    Update query-productos

--- src/query/query-Productos.js
+++ src/query/query-Productos.js
@@ -1,6 +1,13 @@
 /*Tabla Productos*/
 
+/*Nos da todos los productos cuyo ancho sea igual a 0.8 y su estado no sea que no se haya vendido.*/
+db.Productos.find({$and:[
+    {"Caracteristicas.Ancho": {$eq: 0.8}},
+    {"Vendido": {$ne: false}}
+]})
+
+
 /**/
-db.Productos.find({
-    
-})
+db.Productos.find({})
+
+
